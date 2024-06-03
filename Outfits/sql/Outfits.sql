5193787a6fe98651119ff9f33fa08b09e9af1599
Darky_13 <mathieudu13390@hotmail.fr>
2024-06-03 15:05:46 +0200

    Update Outfits.sql

--- Outfits/sql/Outfits.sql
+++ Outfits/sql/Outfits.sql
@@ -1,31 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1
--- Généré le : sam. 25 mai 2024 à 01:24
--- Version du serveur : 10.4.32-MariaDB
--- Version de PHP : 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données : `vorp`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `items`
---
 
 CREATE TABLE IF NOT EXISTS `items` (
   `item` varchar(50) NOT NULL,
@@ -40,46 +12,9 @@ CREATE TABLE IF NOT EXISTS `items` (
   `desc` varchar(5550) NOT NULL DEFAULT 'nice item'
 ) ;
 
---
--- Déchargement des données de la table `items`
---
-
 INSERT ignore INTO `items` (`item`, `label`, `limit`, `can_remove`, `type`, `usable`, `id`, `groupId`, `metadata`, `desc`) VALUES
 ('Outfit', 'Outfit', 1, 1, 'item_standard', 1, 2023, 1, '{}', 'Outfit');
 
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `items`
---
-ALTER TABLE `items`
-  ADD PRIMARY KEY (`item`) USING BTREE,
-  ADD UNIQUE KEY `id` (`id`) USING BTREE,
-  ADD KEY `FK_items_item_group` (`groupId`) USING BTREE;
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `items`
---
 ALTER TABLE `items`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `items`
---
-ALTER TABLE `items`
-  ADD CONSTRAINT `FK_items_item_group` FOREIGN KEY (`groupId`) REFERENCES `item_group` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
